4b8a5e32927aa869dc52bbe1c99c854f2589669d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-05-05 20:53:25 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/firma_responsable/firma_responsable_widget.dart
+++ lib/pages/firma_responsable/firma_responsable_widget.dart
@@ -4,7 +4,6 @@ import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/pages/mensaje_exito/mensaje_exito_widget.dart';
 import '/custom_code/widgets/index.dart' as custom_widgets;
-import '/index.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -227,9 +226,6 @@ class _FirmaResponsableWidgetState extends State<FirmaResponsableWidget> {
                               );
                             },
                           ).then((value) => safeSetState(() {}));
-
-                          context
-                              .pushNamed(LecturaCedulaVecinoWidget.routeName);
                         } else {
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
